debfc3a15ae90daa6f8ae56821ccd93e96703caf
natalie <natgiselle29@gmail.com>
2026-04-30 02:53:27 -0400

    creates first main function in C++

---
+++ cop3502c_intro/program_1.cpp
@@ -0,0 +1,33 @@
+/*
+the same token consist of bunch of functions each serving a purpose
+main is like the power button of the tv
+
+case-sensitive
+*/
+
+/*
+multi-line
+comment
+*/
+
+// this is a single line comment
+
+
+/*
+name of file in the standard c++ library input output stream
+has capabilities of printing something on the screen or getting input from the user
+
+standard library (std) and different files each serving a purpose
+character out (cout) output one or more chars on the screen
+just like java you terminate a line of code with ; at the end
+*/
+#include <iostream>
+
+//specify the type of value that the function is going to return
+// white space doesnt matter but it should be one space in between
+int main() {
+    std::cout << "Hello World :3 !";
+    return 0; // tells the opersting systems that our program is going to terminate correctly
+    // encounters an error if it returns anything + or -
+}
+
