46df8abdf19d24c3e08f3740ff168d9998f6beff
natalie <natgiselle29@gmail.com>
2026-05-02 14:27:18 -0400

    implements C++ variables logic

---
+++ cop3502c_intro/variables.cpp
@@ -0,0 +1,14 @@
+/* VARIABLES:
+    are used to temporarily store data in the computer's memory
+    name of a location in memory where we can store value and can be mutable
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int file_size = 100; 
+    double sales = 9.99;
+    cout << file_size; // prints file_size
+}
+
