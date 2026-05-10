091702f5ada8991f69e7f8dde6d9ec6625801208
natalie <natgiselle29@gmail.com>
2026-05-10 15:54:34 -0400

    implements data_types to compile data_types.cpp file

--- cop3502c_intro/data_types.cpp
+++ cop3502c_intro/data_types.cpp
@@ -37,7 +37,7 @@ int main() {
     double price = 99.99;
     float interest_rate = 3.67F; // will try to store a double inside a flot variable if it is mising f or F at the end which can cause data loss
     long file_size = 90000L; // to force the compiler to treat this as a long needs l or L
-    char letter = ''; // use single quotes to represent a character
+    char letter = 'h'; // use single quotes to represent a character
     bool is_valid = false;
     auto what_is_this = "hi"; // auto data type allows for it to figure out what it is
     auto file_size_2 = 90000; // without the L or l at the end it is treated as an int by the compiler
@@ -46,8 +46,9 @@ int main() {
     int number = 1.2; // is NOT an error it just preserves the integer part
     cout << number;
 
-    int number_2 {}; // brace initalization is 0 without anything
+    int number_2 {0}; // brace initalization is 0 without anything
+    cout << number_2;
     int number_3; 
-    cout number_3 // gives us a random value everytime we run our program if it is not initialized to zero directly or brace intialization
+    cout << number_3; // gives us a random value everytime we run our program if it is not initialized to zero directly or brace initialization
     return 0;
 }
