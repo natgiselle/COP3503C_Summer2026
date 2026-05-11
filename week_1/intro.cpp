6073623e01e71baef4eb97d24c328378f8ed7e2c
natalie <natgiselle29@gmail.com>
2026-05-11 09:47:03 -0400

    adds introduction

---
+++ week_1/intro.cpp
@@ -0,0 +1,18 @@
+/* Intro to C++ (05/11/2025)
+    C++ version 23 is the latest version
+    C++ contains almost all of C
+    Can also type printf in C++ language (& cout)
+    is backward compatible with C++ language
+
+    Focuses on performance + abstraction
+
+    Top C++ Apps:
+    Games
+    
+    Python is used with Machine Learning and Data Science it has great libraries
+    libraries can be imported and you can build on top of it
+
+    C++ is most efficient
+    Python is best for prototyping
+
+*/
