939aab8a40f477c324d66337b13b9f94a8dfe1d6
natalie <natgiselle29@gmail.com>
2026-05-13 23:37:48 -0400

    completes codio functions activities

--- codio_practice/functions.cpp
+++ codio_practice/functions.cpp
@@ -1,19 +1,31 @@
-/* FUNCTIONS BASICS:
-    void- no return value
-    FunctionName = Capitalize the beginning of each letter for typical C++ naming conventions
-
-    example function that does not return anything:
-
-    void SayHi(parameter_1, parameter_2){
-        ...
-    }
+/** FUNCTIONS BASICS:
+*  void- no return value
+*   FunctionName = Capitalize the beginning of each letter for typical C++ naming conventions
+*    example function that does not return anything:
+*
+*   void SayHi(parameter_1, parameter_2){
+*       ...
+*    
+*   @author Natalie Ortiz
+*   @version 1.0
+*   }
 */
-
 #include <iostream>
 using namespace std;
 
 // error is produced if you only try to run this without a main function
 // creating this function alone without putting anything into the main method WILL NOT cause C++ to run it!
+
+
+// PROPER COMMENTING BELOW 
+
+/**
+ * 
+ * 
+ * 
+ *
+ * 
+ */
 
 /**
  * This function greets the user twice
@@ -33,11 +45,30 @@ void SayThanks() {
     cout << "Thanks" << endl;
 }
 
+void PrintActors(){
+    // can add endl in the middle of character out does not have to be strictly at the end
+    cout << "Ben Affleck" << endl;
+    cout << "Kevin Bacon" << endl;
+    cout << "George Clooney" << endl;
+}
+
 int main()
 {
     // calling it n times repeats whatever the function does n times here it prints "Hello" 4 times bcs each call prints "Hello" twice
+    std::cout << "Bye Once Greet Twice Function (x2):" << std::endl;
     GreetTwice();
+    cout << endl;
     GreetTwice();
+    cout << "\n";
+    std::cout << "Say Thanks Function:" << std::endl;
     SayThanks();
+    cout << "\n";
+    std::cout << "Print Actors Function:" << std::endl;
+    PrintActors();
+    cout << "\n";
+
+
+
+
     return 0;
 }
