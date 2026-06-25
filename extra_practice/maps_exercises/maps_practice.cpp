fb83963b7f31b39cc2ef71898e12b228c80772c8
natalie <natgiselle29@gmail.com>
2026-06-25 09:49:13 -0400

    creates myMap map that has a key and holds a value

---
+++ extra_practice/maps_exercises/maps_practice.cpp
@@ -0,0 +1,23 @@
+/** 
+ * MAPS:
+ * std::map is a container that stores key-value pairs
+ * 
+ * similar to that of a dictionary data type from python
+ * 
+ * you search for an item in a map by its key and get value back
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+
+/** REMINDER: whenever you use anything from an included library you must do std:: for anything from it */
+int main() {
+    // must provide type of key, value when creating a map
+    // myMap is a map that has a key of type string and value of type string
+    std::map<std::string, std::string> myMap;
+
+    myMap["name"] = "Pikachu"; // key = "name" and we are creating value of "Natalie"
+    std::cout << myMap["name"] << std::endl;
+    return 0;
+}
