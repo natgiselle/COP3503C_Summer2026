7d6f24d5be23793315e7d5d56efba57e35475f3c
natalie <natgiselle29@gmail.com>
2026-06-29 10:26:25 -0400

    creates linked list class & struct node

--- lecture/linked_lists/more-linked_list.cpp
+++ lecture/linked_lists/more-linked_list.cpp
@@ -1,2 +1,45 @@
 
+// last node always points to null ptr
+// head 1->2->3 tail
+#include <iostream>
+using namespace std;
+class LinkedList{
+    private:
+    struct Node {
+        int data;
+        Node* next;
+        Node(int d): data(d), next(nullptr) {}
+    };
+    Node* head;
+    public: 
 
+    LinkedList(): head(nullptr) {} // constructor
+    ~LinkedList(){
+        Node* current = head;
+        
+        while(current != nullptr){
+            Node* temp = current->next;
+            delete current;
+            current = temp;
+        }
+    }
+    void insertTail(int data_){
+        Node* newNode = new Node(data_);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* current = head;
+        while(current->next != nullptr){
+            current = current->next;
+        }
+    }
+
+    void removeHead() {
+        if(head == nullptr){ // handles empty case if head is nullptr there is nothing in the linked list
+            return;
+        }
+    }
+
+
+};
