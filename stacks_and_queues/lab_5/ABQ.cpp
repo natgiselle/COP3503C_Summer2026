f4e1194bd2cc6404c93a72db0cd48f2c33d3f3ea
natalie <natgiselle29@gmail.com>
2026-07-02 21:11:53 -0400

    fixes typo ABS -> ABQ

--- stacks_and_queues/lab_5/ABQ.cpp
+++ stacks_and_queues/lab_5/ABQ.cpp
@@ -6,40 +6,39 @@
 using namespace std;
 
 template <typename T>
-class ABS {
+class ABQ {
 
-    // implement your class
     private:
-        T* list_;
-        unsigned int capacity_;
-        unsigned int size_;
-        float scale_;
+        // implement your class
+        T* list_; // ptr to dynamic array holding queue data
+        unsigned int capacity_; // max # of elements the array can hold
+        unsigned size_; // current # of elements in the queue
+        float scale_; // resize factor using 2.0f
+
     public:
+        ABQ();                          // Default constructor
+        ABQ(int capacity);              // Constructor with starting capacity
+        ABQ(const ABQ& d);              // Copy constructor
+        ABQ& operator=(const ABQ& d);   // Copy assignment operator
+        ~ABQ();                         // Destructor
 
-        ABS();
-        ABS(unsigned int capacity);
-        void resize(unsigned int newCapacity);
-        ABS(const ABS& d);
-        ABS& operator=(const ABS& d);
-        ~ABS();
+        void enqueue(T data);           // Add an item to the back of the queue
+        T dequeue();                    // Remove and return the front item
+        T peek();                       // Return the front item without removing it
 
-        void push(T data);
-        T pop();
-        T peek();
-        
-        unsigned int getSize();
-        unsigned int getMaxCapacity();
-        T* getData();
+        unsigned int getSize();         // Return current number of stored items
+        unsigned int getMaxCapacity();  // Return current array capacity
+        T* getData();                   // Return pointer to the internal array
 };
 
 // destructor
     template <typename T>
-    ABS<T>::~ABS(){
+    ABQ<T>::~ABQ(){
         delete[] list_; // delete the dynamically allocated array (pointer to an array called list_ of type T)
     }
 
 // default constructor by doing list initializing
     template <typename T>
-    ABS<T>::ABS(): capacity_(1), size_(0), scale_(2.0f){
+    ABQ<T>::ABQ(): capacity_(1), size_(0), scale_(2.0f){
         list_ = new T[capacity_];
     }
