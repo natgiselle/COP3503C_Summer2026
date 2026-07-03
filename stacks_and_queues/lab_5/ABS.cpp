511029ffd7e2236b7d4b3bb0585512df710a809c
natalie <natgiselle29@gmail.com>
2026-07-02 20:55:08 -0400

    creates private-member variables for ABS: Array-Based Stack class

---
+++ stacks_and_queues/lab_5/ABS.cpp
@@ -0,0 +1,15 @@
+    /** 
+     * ARRAY-BASED STACK:
+    */
+
+#include <iostream>
+using namespace std;
+template <typename T>
+class ABS {
+
+    private:
+        T* list_;
+        unsigned int capacity_;
+        unsigned int size_;
+        float scale_;
+};
