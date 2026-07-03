8b5275fd392058a0475f507c95f6ef5b9228bbf9
natalie <natgiselle29@gmail.com>
2026-07-02 21:08:27 -0400

    create private-member variables for ABQ: Array-Based Queue class

---
+++ stacks_and_queues/lab_5/ABQ.cpp
@@ -0,0 +1,17 @@
+/**
+ * ARRAY-BASED QUEUE:
+*/
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class ABS {
+
+    // implement your class
+    private:
+        T* list_;
+        unsigned int capacity_;
+        unsigned int size_;
+        float scale_;
+};
