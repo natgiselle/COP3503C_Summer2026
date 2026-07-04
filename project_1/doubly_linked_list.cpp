5476c3a98c5446f706ada417be737f45f9941ff1
natalie <natgiselle29@gmail.com>
2026-07-03 23:57:25 -0400

    creates AddHead function

--- project_1/doubly_linked_list.cpp
+++ project_1/doubly_linked_list.cpp
@@ -16,4 +16,21 @@ class LinkedList{
         Node* head; // first node in array (first)
         Node* tail; // end node in array (last)
         unsigned int count; // # nodes in array
-};
+
+    public:
+        void AddHead(const T& value){
+        Node* newNode = new Node(); // allocate memory for a new node
+        newNode->data = value; // set newnode data attribute to equal value given
+        newNode->prev = nullptr; // set previous node to be nullptr meaning the one before it should be what indicates thestart and the head is right aftert nullptr
+        newNode->next = head; // set the one right after the current node holding data to equal old head
+        if(head != nullptr){ // if the head is NOTalready nullptr that just means you make the previous one before that head value to equal the newnode since that means that the doubly linked list is not empty
+            head->prev = newNode;
+        }
+        if(head == nullptr){ // if the doubly linked list is empty you set tail equal to newNode because head being null means there is not starting value so thats why we just make the tail equal to the value we have and still have next ewual to null pte therefore making the ending nullptr and keeping the beginning nullptr
+            tail = newNode; // the new node is now the tail
+        }
+        head = newNode;
+        count++; // because we are adding a new node we have to increase count anytime there is new node added to give accurate # of the amount of nodes/elements in the doubly linked list
+        }
+
+}; 
