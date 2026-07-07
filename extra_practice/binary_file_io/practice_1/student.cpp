2f674144b7c1d79e02b4f762167f38b73765b48f
natalie <natgiselle29@gmail.com>
2026-07-07 11:43:03 -0400

    implement Student struct

---
+++ extra_practice/binary_file_io/practice_1/student.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Student {
+    char name[20];
+    int id;
+    double gpa;
+};
+
+
+int main(){
+    // file writing 
+    Student s1; // create an instance of the Student struct (object)
+    
+    
+    
+    
+    
+    return 0;
+}
