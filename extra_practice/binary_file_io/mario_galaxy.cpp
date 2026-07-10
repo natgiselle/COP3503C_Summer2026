dd45d8de0c99fe5877c7240d997f169e867d4054
natalie <natgiselle29@gmail.com>
2026-07-10 03:21:20 -0400

    completes super mar-IO galaxy practice

--- extra_practice/binary_file_io/mario_galaxy.cpp
+++ extra_practice/binary_file_io/mario_galaxy.cpp
@@ -35,6 +35,7 @@
  */
 #include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
 using namespace std;
 
@@ -57,12 +58,17 @@ int main(){
     ofstream savefiledata("../out.o", ios::binary); // ensures that it writes in binary
 
     if(savefiledata.is_open()){
-        for(unsigned int i = 0; i < galaxyRec1.size(); i++){
+        int count = galaxyRec1.size();
+        savefiledata.write((char*)&count, sizeof(count));
+        for(int i = 0; i < galaxyRec1.size(); i++){
+            int nameLen = galaxyRec1[i].galaxyName.size();
+            savefiledata.write((char*)&nameLen, sizeof(nameLen));
             // creates new binary object that holds the galaxyName attribute of galaxyRecord struct object at index i in galaxyRec1 vector
-            savefiledata.write((char*)&(galaxyRec1[i].galaxyName), (galaxyRec1[i].galaxyName).size());
+            savefiledata.write(galaxyRec1[i].galaxyName.c_str(), (galaxyRec1[i].galaxyName).size());
 
             // creates a new binary object that holds the starsCollected attribute of galaxyRecord struct object at index i in galaxyRec1 vector
             savefiledata.write((char*)&(galaxyRec1[i].starsCollected), sizeof(galaxyRec1[i].starsCollected));
+
         }
     }
     else {
@@ -72,22 +78,31 @@ int main(){
     
     savefiledata.close();
 
+
+
+
     ifstream readfiledata("../out.o", ios::binary); // one @param which is which file we are reading
 
-    if(readfiledata.is_open()){
-        int countLength = galaxyRec1.size();
-        vector<GalaxyRecord> galaxyRec2;
-        int totalStars = 0;
 
+    int countLength;
+    readfiledata.read((char*)&countLength, sizeof(countLength));
+
+    vector<GalaxyRecord> galaxyRec2;
+
+    int totalStars = 0;
+
+    if(readfiledata.is_open()){
         cout << "====== Mario's Save File ======" << endl;
         // loop 1 to read the attributes of the struct objects, initialize  attributes inside forloop and resize any string attributes and add the objects to the newly made vector
-        for(unsigned int i = 0; i < countLength; i++){
+        for(int i = 0; i < countLength; i++){
             int nameLen; // 1. create a namelen attribute
             // 2. reads from file the string galaxyName attribute of the galaxyRecord struct object at index i of galaxyRec1 vector
-          readfiledata.read((char*)&nameLen, sizeof(nameLen)); // bytes of len
+            readfiledata.read((char*)&nameLen, sizeof(nameLen)); // bytes of len
 
             string name; // 3. create a new string since we do not use the already existing values when reading from a file
             name.resize(nameLen); // 4. must resize for strings
+            readfiledata.read(name.data(), nameLen);
+
             // reads from file the int starsCollected attribute of the galaxyRecord struct object at index i of galaxyRec1 vector
             int stars;
             readfiledata.read((char*)&stars, sizeof(int));
@@ -95,7 +110,7 @@ int main(){
             galaxyRec2.push_back({name, stars});
         }   
         // loop 2 to print from the second vector
-        for(unsigned int i = 0; i < countLength; i++){
+        for(int i = 0; i < countLength; i++){
             cout << "\t" << galaxyRec2[i].galaxyName << ": "; // 2. print out newly created string 
             
             cout << " " << galaxyRec2[i].starsCollected << "★" << endl;
