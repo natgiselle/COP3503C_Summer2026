84b1141cc1b89186bc7c780c890e044953d7f100
natalie <natgiselle29@gmail.com>
2026-07-10 01:54:36 -0400

    adds vector<galaxyRecords> galaxyRec1 and pushes GalaxyRecord objects into it

--- extra_practice/binary_file_io/mario_galaxy.cpp
+++ extra_practice/binary_file_io/mario_galaxy.cpp
@@ -1,3 +1,6 @@
+// Practice exercise for binary file I/O: Super Mario Galaxy references
+// used purely as example data, not affiliated with Nintendo
+
 /** SUPER BINARY MAR-IO: GALAXY 1
  * 
  *  OBJECTIVE: Simulate a simplified save file system that tracks which galaxies Mario has visited
@@ -20,27 +23,35 @@
  *  - Read the galaxy count first.
  *  - Loop that many times reconstructing each GalaxyRecord into a fresh vector
  * 
- * 5. ===== Mario's Save File =====
+ * 5.
+ *   ====== Mario's Save File ======
  *      Good Egg Galaxy: 6 stars
- *     Honeyhive Galaxy: 5 stars
- *     Loopdeeloop Galaxy: 1 star
- *     Flipswitch Galaxy: 1 star
- *     Total Stars: 13
+ *      Honeyhive Galaxy: 5 stars
+ *      Loopdeeloop Galaxy: 1 star
+ *      Flipswitch Galaxy: 1 star
+ *      Total Stars: 13
  * 
  * 
  */
 #include <iostream>
+#include <string>
 #include <fstream>
 using namespace std;
 
 struct GalaxyRecord {
-
+    string galaxyName;
+    int starsCollected;
 };
 
-
 int main(){
-
-
+    vector<GalaxyRecord> galaxyRec1;
+        galaxyRec1.push_back({"Good Egg Galaxy", 6});
+        galaxyRec1.push_back({"Honeyhive Galaxy", 5});
+        galaxyRec1.push_back({"Loopdeeloop Galaxy", 1});
+        galaxyRec1.push_back({"Flipswitch Galaxy", 1});
+    // can use StructName ObjName = {attribute 1, attribute 2}; to initalize a struct object with its attributes already declared with values 
+    // galaxyRec1.galaxyName = "Good Egg Galaxy";
+    // galaxyRec1.starsCollected = 6;
 
     return 0;
 }
