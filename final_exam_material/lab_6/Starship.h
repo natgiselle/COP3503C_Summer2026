cb9a14644b13678fd9d09a4f0dd3bad3db992912
natalie <natgiselle29@gmail.com>
2026-07-10 17:28:14 -0400

    completes print() function

--- final_exam_material/lab_6/Starship.h
+++ final_exam_material/lab_6/Starship.h
@@ -5,11 +5,9 @@
     using namespace std;
 
     // FREEZE CODE BEGIN
-    class Starship
-    {
+class Starship {
     public:
-        struct Weapon
-        {
+        struct Weapon {
             string name;
             int powerRating;
             float energyUse;
@@ -26,11 +24,32 @@
         // Your code goes here.
         // Create the Firepower() and Print() member functions.
         int FirePower(){
-        int totalFirePower = 0;
-        for(int i = 0; i < inventory.size(); i++){
-            totalFirePower += inventory[i].powerRating;
+            int totalFirePower = 0;
+            for(int i = 0; i < inventory.size(); i++){
+                totalFirePower += inventory[i].powerRating;
+            }
+            return totalFirePower;
         }
-        return totalFirePower;
+
+        void Print(){
+            // the member-variables in Starship class are public meaning we can directly access without using getters/parametrized constructor
+            cout << "Name: " << name << endl;
+            cout << "Class: " << shipClass << endl;
+            cout << "Length: " << length << endl;
+            cout << "Shield capacity: " << shieldCapacity << endl;
+            
+            // if the ship has NO weapons
+            if(inventory.size() == 0){
+                cout << "Armaments: Unarmed" << endl;
+            }
+
+            // if the ship has atleast one or more weapons since .size() of a vector can never be negative
+            else {
+                cout << "Armaments: ";
+                for(int i = 0; i < inventory.size(); i++){
+                cout << inventory[i].name << ", " << inventory[i].powerRating << ", " << inventory[i].energyUse  << endl;
+                }
+            }
         }
 
         
