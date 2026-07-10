219029b22162af19b94cdc08a01ea45adb95680f
natalie <natgiselle29@gmail.com>
2026-07-09 23:59:47 -0400

    fixes error by upgrading my compiler to c++17

--- lecture/binary/student_bin.cpp
+++ lecture/binary/student_bin.cpp
@@ -2,6 +2,11 @@ class Student {
     private:
 
 };
+// no converdion needed because th data type is already character
+// how any byts do yoy kneed to wrtite? lenName;
+// cqn do size of int wehich just gives the max amt of bytes of an int to br sble to rad it but typicallt you want ot just do the size of th thing itself 
+
+// ypu dont need (char*) conversion if the thg we are triying to write or rad 
 ofstream fout("student.o", ios::binary){
 
-}
+    }
