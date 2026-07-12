3bc6239afa8ee9ccc0ff8683a349bbbe4d3d0cd6
natalie <natgiselle29@gmail.com>
2026-07-11 21:45:20 -0400

    finishes readlookbinary and readstringbinary

--- extra_practice/final_material/binary_file_io/wears_prada/fashion_closet.cpp
+++ extra_practice/final_material/binary_file_io/wears_prada/fashion_closet.cpp
@@ -18,9 +18,45 @@ void WriteStringBinary(fstream& file, const string& str){
 }
 
 void WriteLookBinary(fstream& file, const string& designerPiece, int seasonYear){
-    int len = designerPiece.size(); // returns the number of characters and we know that 1 char = 1 byte so the length of the string is equal to the amount of bytes it holds
-    file.write((char*)designerPiece.data(), len);
-
+//  returns the number of characters and we know that 1 char = 1 byte so the length of the string is equal to the amount of bytes it holds
+    WriteStringBinary(file, designerPiece);
     file.write((char*)&seasonYear, sizeof(int)); // an int is going to be equal to 4 bytes
+}
+
+
+void ReadStringBinary(fstream& file, string& str){
+    // because we are reading it we are going to be using resize;
+    int len;
+    file.read((char*)&len, sizeof(len));
+
+    str.resize(len);
+    file.read((char*)&str[0], len);
+}
+
+/** 
+ * 
+ * FILEREAD:
+ * IT PHYSICALLY DOES THESE THINGS
+ * 1. it looks at the file's current position
+ * 2. it grabs the next 4 bytes from the file the ones representing 2003
+ * 3. it goes to addresss 0x1000 because thats what &seasonYear gave it and overwrites the garbage in that box with those 4 bytes from the file
+*/
+void ReadLookBinary(fstream& file, string& designerPiece, int& seasonYear){
+    ReadStringBinary(file, designerPiece);
+    /** first @param: the memory address (IN MEMORY, not the file) where file.read will place the bytes it pulls from the file */
+    /** second @param: how many bytes to pull out of the file and copy into that destination */
+    // file.read reads bytes out of it
+
+    // bytes gets copied from one location (the file, on disk) to another location (seasonYear, in memory)
+
+    // seasonYear is a box in your programs memory at addrees 0x1000 for example, its 4 bytes contain garbage, whatevr leftover values happen to be sitting there
+    // the file has 4 specific bytes sitting on disk that represent 2003
+    file.read(reinterpret_cast<char*>(&seasonYear), sizeof(int));
+}
+
+int main() {
+    fstream outFile("runway.bin", ios::binary | ios::out);
+    
+    
 }
 
