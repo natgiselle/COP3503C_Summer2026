fc0305376e594c65fc6a8c34cd794a48808bd8a3
natalie <natgiselle29@gmail.com>
2026-07-22 10:47:34 -0400

    demonstrates example

--- lecture/inheritance/animal.cpp
+++ lecture/inheritance/animal.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
-
+// first usage is resolving the diamond proble where it has double copy of base class
+// second usage is to be able to use a base-class pointer to access vars, member funcs from base-class and inherited classes (derived)
 class Animal {
     private: // private by default 
         string name;
     public:
         Animal(string name_) : name(name_) {}
 
-        void Voice(){
-            cout << "" << endl;
+        void voice(){
+            cout << "Unidentified voice" << endl;
+        }
+        void hungry(){
+            cout << "Hungry ! :p" << endl;
         }
 };
 
@@ -22,4 +26,32 @@ class Cat : public Animal {
         Cat(string name_, int lives_) : Animal(name_) /** , lives(lives_) */ {
             lives = lives_;
         }
+
+        // improper polymorphism 
+        void voice(){
+            cout << "Meow :3" << endl;
+        }
 };
+
+
+class Dog : public Animal {
+    int walks;
+    public:
+        Dog(string name_, int walks_) : Animal(name_), walks(walks_) {}
+
+        void voice(){
+            cout << "Bark :3" << endl;
+        }
+
+};
+
+
+int main(){
+    Animal animal("Animal");
+    Cat cat("Gigi", 9);
+    Dog dog("Thor", 10);
+
+    animal.voice();
+    dog.voice();
+    cat.voice();
+}
