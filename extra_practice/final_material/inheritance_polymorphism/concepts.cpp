a7b1fe67b7885690f377a92225dc372a8947db14
natalie <natgiselle29@gmail.com>
2026-07-21 23:49:21 -0400

    implements Animal classs

---
+++ extra_practice/final_material/inheritance_polymorphism/concepts.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+/** INHERITANCE & POLYMOPRHISM CONCEPTS: */
+
+/** INHERITANCE:
+ * a derived class that resuses and extends a base class
+ */
+class Animal {
+    private:
+        string name;
+    public: 
+    // list initializer for parametrized constructor
+    Animal(string n) : name(n) {}
+}; // NEVER FORGET TO ADD SEMI COLON FOR THE END OF A CLASS!
