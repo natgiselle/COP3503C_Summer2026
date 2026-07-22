9f322cec12c7aeca40d9584d07e885bc8a6d11c0
natalie <natgiselle29@gmail.com>
2026-07-21 23:50:22 -0400

    implements Dog class which inherits attribute for animal

--- extra_practice/final_material/inheritance_polymorphism/concepts.cpp
+++ extra_practice/final_material/inheritance_polymorphism/concepts.cpp
@@ -14,3 +14,8 @@ class Animal {
     // list initializer for parametrized constructor
     Animal(string n) : name(n) {}
 }; // NEVER FORGET TO ADD SEMI COLON FOR THE END OF A CLASS!
+
+class Dog: public Animal {
+    public: 
+        Dog(string n) : Animal(n) {}
+};
