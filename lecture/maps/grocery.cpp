ec751bd65820113ebbe63c7735596042500ca10e
natalie <natgiselle29@gmail.com>
2026-07-29 09:51:06 -0400

    add fruits to groceries

---
+++ lecture/maps/grocery.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+
+int main(){ 
+    std::unordered_map<std::string, int> groceries; // unordered_map is non-contigiuous
+    groceries.emplace("banana",3);
+    groceries["dragonfruit"] = 5;
+    groceries["lime"]= 7;
+
+
+    // unordered_map iterator for unordered_map map and it will print it NOT sorted
+    // map iterator for map map it will print it sorting the keys in alphabetical order
+    // map.end() similar to  != nullptr
+    // wehneve you use regular map data structure, you must loop through 
+
+    // can use auto instead when defining something in a loop
+    for(std::unordered_map<std::string, int>::iterator it = groceries.begin(); it != groceries.end(); ++it){
+        std::cout << it->first << ": " << it->second << std::endl; 
+    }
+    return 0;
+}
