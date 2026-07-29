201e4ce736efd0876d074d0378aa9537d5cab66b
natalie <natgiselle29@gmail.com>
2026-07-29 10:08:07 -0400

    implements auto

---
+++ lecture/functors/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+
+/** 
+ * AUTO:
+ * function names are pointers to the set of instructions so it will adjust to that type for auto
+ * 
+ * char = 50 char can be a number value like this for ASCII value it could be aspcific letter or symbol etc
+ *
+ * 
+ * BEST FOR ITERATORS
+ * THE WORST FOR BASIC DATA TYPES IT MAKES THE CODE LESS READABLE AND MORE CONFUSING
+*/
