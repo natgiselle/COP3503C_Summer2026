365b7a90e21ce0f8c5505bbc03169a9baa7888be
natalie <natgiselle29@gmail.com>
2026-07-30 17:17:42 -0400

    implements changes to functino file

--- lecture/functors/function.cpp
+++ lecture/functors/function.cpp
@@ -23,8 +23,8 @@ int func(){
     return 5;
 }
 
-int func2(){
-    return 6;
+int func2(int a, int b){
+    return a+b;
 }
 
 int main(){
@@ -32,5 +32,20 @@ int main(){
     cout << "func call: " << func() << endl;
     cout << "func name: " << func << endl;
     cout <<"func memory address: "<< (void*) func << endl; // gives memory address of the function cast it to void asterisk, it prints 1 for any func with just function name
+    
+    int a = 4;
+    int* ptr = &a;
+
+    // derefrence 
+    int(*funcPtr)() = func; // pointer to the function, same list of parameter and the return type and some name
+    
+        cout << (void*)funcPtr << endl;
+
+        // remove name of paramters but keep the type
+
+        int(*func2Ptr)(int, int) = func2;
+
+        cout << "original: " << (void*) func2 << endl;
+        cout << func2Ptr(5,6) << endl; // have alias to actual function name;
     return 0;
 }
